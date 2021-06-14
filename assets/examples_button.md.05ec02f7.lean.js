8601867b7de0e0ccdc62526adc2426beabecea06
louruixiao <775674502@qq.com>
2021-06-14 07:32:07 +0000

    Deploying to gh-pages from @ louruixiao/gavin-ui-docs@e331974032ec2d7921e364fe3ea9c678a6aef9e3 🚀

---
+++ assets/examples_button.md.05ec02f7.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as e}from"./app.a4c66706.js";const r='{"title":"","description":"","frontmatter":{},"relativePath":"examples/button.md","lastUpdated":1623655860217}',a={};a.render=function(r,a,o,n,p,d){return t(),e("div")};export default a;export{r as __pageData};
